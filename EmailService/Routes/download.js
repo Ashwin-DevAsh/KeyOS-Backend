3d237a4d99bea95db1ba067afbba18284e316223
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-11-11 05:41:59 +0530

    added foreground service

--- EmailService/Routes/download.js
+++ EmailService/Routes/download.js
@@ -2,7 +2,7 @@ var app = require("express").Router();
 var jwt = require("jsonwebtoken");
 
 app.get("/download", function (req, res) {
-  const file = "../public/Apk/demo.txt";
+  const file = "../public/Apk/KeyOS.apk";
   res.download(file);
 });
 
