2387c9aeebe96b1484c4c1e2d32485a2ae78f4d9
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-11-29 15:57:30 +0530

    add keyos console

---
+++ KeyOS-Console/src/assets/Images.ts
@@ -0,0 +1,49 @@
+import keyOS from "../assets/keyOS.svg";
+import mobile from "../assets/mobile.svg";
+import ovelrect from "../assets/ovelRect.svg";
+import work from "../assets/work.svg";
+import apps from "./apps.svg";
+import browser from "./chrome.svg";
+import paint from "./paint.svg";
+import mobileFeatures from "./mobile_features.svg";
+import featureKioskMode from "./feature_kiosk_mode.svg";
+import featureWebBrowser from "./feature_web_browser.svg";
+import featureCallBlocker from "./feature_call_blocker.svg";
+import featureCustomize from "./feature_customize.svg";
+import featureEditApp from "./feature_edit_app.svg";
+import featureSettings from "./feature_settings.svg";
+import googlePlayBadge from "./google-play-badge.png";
+import phoneWithHands from "./phone_with_hands.svg";
+import appUsers from "./appUsers.webp";
+import burgarMenu from "./burgar_menu.svg";
+import ovalRectBlue from "./oval_rect_blue.svg";
+import arrow from "./arrow-icon.svg";
+import leftArrow from "./left-arrow.svg";
+import rightArrow from "./right-arrow.svg";
+import star from "./star.svg";
+
+export default {
+  keyOS,
+  mobile,
+  ovelrect,
+  work,
+  apps,
+  browser,
+  paint,
+  mobileFeatures,
+  featureKioskMode,
+  featureWebBrowser,
+  featureCallBlocker,
+  featureCustomize,
+  featureEditApp,
+  featureSettings,
+  googlePlayBadge,
+  phoneWithHands,
+  appUsers,
+  burgarMenu,
+  ovalRectBlue,
+  arrow,
+  leftArrow,
+  rightArrow,
+  star,
+};
