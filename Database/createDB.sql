f1b739fb15c3d6bcd8dc9e24216edf431b574afe
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-12-25 21:02:55 +0530

    Added database

---
+++ Database/createDB.sql
@@ -0,0 +1,11 @@
+create table users(
+      deviceID varchar,
+      brand varchar,
+      model varchar,
+      sdk varchar,
+      versionName varchar,
+      isLaunched varchar,
+      config json
+);
+
+
