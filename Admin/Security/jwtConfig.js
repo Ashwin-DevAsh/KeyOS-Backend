da5084d543d83285af0b07a440604dca5152d01a
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-12-26 10:21:34 +0530

    Add private key

--- Admin/Security/jwtConfig.js
+++ Admin/Security/jwtConfig.js
@@ -1,12 +1,11 @@
-module.exports = JwtConfig = (req, res, next) => {
+module.exports = JwtConfig = async (req, res, next) => {
   console.log("Middle ware");
   try {
     await jwt.verify(req.get("token"), process.env.PRIVATE_KEY).id;
-    next()
+    next();
   } catch (e) {
     console.log(e);
     res.send({ message: "invalid admin" });
     return;
   }
-
 };
