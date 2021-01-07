5cfa7f28eb50f1daf82a144237d44c851dbf127e
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-01-08 01:32:09 +0530

    loading issue fix

--- Console/src/App.tsx
+++ Console/src/App.tsx
@@ -5,31 +5,24 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Images from "./assets/Images";
 
 function App() {
-  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [loaderClass, setLoaderClass] = React.useState("loader");
 
   React.useEffect(() => {
-    for (var i in Images) {
-      const img = new Image();
-      img.src = i;
-    }
     setTimeout(() => {
-      setIsLoaded(true);
+      setLoaderClass(loaderClass + " makeinvisible");
     }, 1500);
   }, []);
 
   return (
     <div className="App">
-      {isLoaded ? (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={HomePage}></Route>
-          </Switch>
-        </Router>
-      ) : (
-        <div className="loader">
-          <img style={{ height: 80, width: 80 }} src={Images.logo} />
-        </div>
-      )}
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+        </Switch>
+      </Router>
+      <div className={loaderClass}>
+        <img style={{ height: 80, width: 80 }} src={Images.logo} />
+      </div>
     </div>
   );
 }
