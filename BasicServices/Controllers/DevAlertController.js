15c66fa5e45b2d0e4713c55015ec255d629d9f14
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-01-15 06:35:26 +0530

    reformat json

--- BasicServices/Controllers/DevAlertController.js
+++ BasicServices/Controllers/DevAlertController.js
@@ -66,7 +66,7 @@ class DevAlertController {
         if (element.length != 0)
           output = this.objectRecussion(
             element,
-            `${output + space}${key} <br>`,
+            `${output + space} <br> ${key} <br>`,
             space + space
           );
       } else {
