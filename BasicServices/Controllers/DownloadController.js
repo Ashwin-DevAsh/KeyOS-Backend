de5be7ec857a2918d2e309bae4eeb28d0a27f131
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-01-17 04:14:30 +0530

    fix bugd

--- BasicServices/Controllers/DownloadController.js
+++ BasicServices/Controllers/DownloadController.js
@@ -4,11 +4,6 @@ class DownloadContorller {
   emailService = new EmailService();
 
   downloadProApk = (req, res) => {
-    this.emailService.sendMail(
-      "Update Apk",
-      "Pro apk download request",
-      this.emailService.devEmail
-    );
     const file = "../public/Apk/KeyOS.apk";
     res.download(file);
   };
