b4d35dc95b8c029dc1a989817596f16660176ae1
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-07-05 10:10:52 +0530

    env

--- BasicServices/Controllers/DonationController.js
+++ BasicServices/Controllers/DonationController.js
@@ -1,11 +1,21 @@
 var DonationService = require("../Services/DonationService");
+const EmailService = require("../Services/EmailService");
 
 class DonationController {
   donationService = new DonationService();
+  emailService = new EmailService();
+
 
   createOrder = async(req, res) => {
     const {amount,id} = req.body
     var orderID = await this.donationService.createOrder(amount,id)
+    this.emailService.sendMail(
+        `New Donation Created`,
+        `<h1>${id}</h1><br/>` +
+           `Amount = ${amount}` +
+          "<br/>" ,
+        this.emailService.devEmail
+    );
     res.send({orderID:orderID.id,keyID:process.env.key_id})
   };
 }
