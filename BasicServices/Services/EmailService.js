0555a8d72e3d624789aecb0620c6dcb579072929
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-09-12 14:04:27 +0530

    admin.keyos.in

--- BasicServices/Services/EmailService.js
+++ BasicServices/Services/EmailService.js
@@ -15,7 +15,7 @@ module.exports = class EmailService {
 
   sendMail = async (subject, text, toEmail) => {
     var mailOptions = {
-      from: `KeyOS <keyos.devash@gmail.com>`,
+      from: `KeyOS <no-replay@keyos.in>`,
       to: toEmail,
       subject: subject,
       html: text,
