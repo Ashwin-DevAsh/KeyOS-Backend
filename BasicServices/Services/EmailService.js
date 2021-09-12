db3f727b6e4c5c41e4347d3a9d8ea90a529aff2c
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-09-12 14:11:42 +0530

    admin.keyos.in

--- BasicServices/Services/EmailService.js
+++ BasicServices/Services/EmailService.js
@@ -6,7 +6,7 @@ module.exports = class EmailService {
   devEmail = "2017devash@gmail.com";
 
   transporter = nodemailer.createTransport({
-    host: 'smtp.zoho.eu',
+    host: 'smtp.zoho.in',
     port: 465,
     secure: true, // use SSL
     auth: {
