a1ae20414188592eae7dd2bd4d1e2d2585107d80
Ashwin-DevAsh <2017ashwin@gmail.com>
2022-02-20 22:07:59 +0530

    added cloud

--- BasicServices/Routes/download.js
+++ BasicServices/Routes/download.js
@@ -7,7 +7,7 @@ var downloadController = new DownloadController();
 app.get("/download", downloadController.downloadProApk);
 app.get("/v1/download", downloadController.downloadProApk);
 
-app.get("/cc", downloadController.cc);
+app.get("/lab/cc", downloadController.cc);
 
 
 module.exports = app;
