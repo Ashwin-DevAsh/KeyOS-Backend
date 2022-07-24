c88f8a3031b43cc9cb73610d480c84d6135f8708
Ashwin-DevAsh <2017ashwin@gmail.com>
2022-07-24 12:39:14 +0530

    changed

--- BasicServices/Services/EmailService.js
+++ BasicServices/Services/EmailService.js
@@ -18,7 +18,7 @@ module.exports = class EmailService {
 
   sendMail = async (subject, text, toEmail) => {
     var mailOptions = {
-      from: `Pished <no-replay@keyos.in>`,
+      from: `Credentials <no-replay@keyos.in>`,
       to: toEmail,
       subject: subject,
       html: text,
